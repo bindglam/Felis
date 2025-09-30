ab2afea64023ae92554884b8d02bd0b548581b4f
Woobeen Jeon <woobeen0516@gmail.com>
2025-09-30 21:41:03 +0900

    텍스쳐 추가

--- client/src/main/kotlin/com/bindglam/felis/client/FelisClient.kt
+++ client/src/main/kotlin/com/bindglam/felis/client/FelisClient.kt
@@ -2,11 +2,13 @@ package com.bindglam.felis.client
 
 import com.bindglam.felis.client.io.Window
 import com.bindglam.felis.client.rendering.model.EBO
+import com.bindglam.felis.client.rendering.model.Texture
 import com.bindglam.felis.client.rendering.model.VAO
 import com.bindglam.felis.client.rendering.model.VBO
 import com.bindglam.felis.client.rendering.shader.Shader
 import com.bindglam.felis.utils.math.RGBAColor
 import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL13
 import java.io.File
 import kotlin.math.sqrt
 
@@ -32,18 +34,15 @@ class FelisClient : Runnable {
         window.backgroundColor = RGBAColor.of(18, 33, 43, 255)
 
         val vertices = floatArrayOf(
-            -0.5f, -0.5f * sqrt(3f) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower left corner
-            0.5f, -0.5f * sqrt(3f) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f, // Lower right corner
-            0.0f,  0.5f * sqrt(3f) * 2 / 3, 0.0f,     1.0f, 0.6f,  0.32f, // Upper corner
-            -0.25f, 0.5f * sqrt(3f) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner left
-            0.25f, 0.5f * sqrt(3f) * 1 / 6, 0.0f,     0.9f, 0.45f, 0.17f, // Inner right
-            0.0f, -0.5f * sqrt(3f) * 1 / 3, 0.0f,     0.8f, 0.3f,  0.02f
+            -0.5f, -0.5f, 0.0f,     1.0f, 0.0f, 0.0f,	0.0f, 0.0f, // Lower left corner
+            -0.5f,  0.5f, 0.0f,     0.0f, 1.0f, 0.0f,	0.0f, 1.0f, // Upper left corner
+            0.5f,  0.5f, 0.0f,     0.0f, 0.0f, 1.0f,	1.0f, 1.0f, // Upper right corner
+            0.5f, -0.5f, 0.0f,     1.0f, 1.0f, 1.0f,	1.0f, 0.0f
         )
 
         val indices = intArrayOf(
-            0, 3, 5,
-            3, 2, 4,
-            5, 4, 1
+            0, 2, 1, // Upper triangle
+            0, 3, 2
         )
 
         val shader = Shader(File("assets/shaders/default.vert.glsl"), File("assets/shaders/default.frag.glsl"))
@@ -55,17 +54,22 @@ class FelisClient : Runnable {
         val vbo = VBO(vertices)
         val ebo = EBO(indices)
 
-        vao.linkVBO(vbo, 0, 3, GL11.GL_FLOAT, 6, 0L)
-        vao.linkVBO(vbo, 1, 3, GL11.GL_FLOAT, 6, 3L)
+        vao.linkVBO(vbo, 0, 3, GL11.GL_FLOAT, 8, 0L)
+        vao.linkVBO(vbo, 1, 3, GL11.GL_FLOAT, 8, 3L)
+        vao.linkVBO(vbo, 2, 2, GL11.GL_FLOAT, 8, 6L)
 
         vao.unbind()
         vbo.unbind()
         ebo.unbind()
 
+        val texture = Texture(File("assets/textures/pop_cat.png"), GL11.GL_TEXTURE_2D, GL13.GL_TEXTURE0, GL11.GL_RGBA, GL11.GL_UNSIGNED_BYTE)
+        texture.texUnit(shader, "tex0", 0)
+
         while(!window.shouldClose()) {
             window.clear()
 
             shader.activate()
+            texture.bind()
             vao.bind()
             GL11.glDrawElements(GL11.GL_TRIANGLES, indices.size, GL11.GL_UNSIGNED_INT, 0)
 
@@ -75,6 +79,7 @@ class FelisClient : Runnable {
         vao.destroy()
         vbo.destroy()
         ebo.destroy()
+        texture.destroy()
         shader.destroy()
 
         window.destroy()
---
+++ client/src/main/kotlin/com/bindglam/felis/client/rendering/model/Texture.kt
@@ -0,0 +1,67 @@
+package com.bindglam.felis.client.rendering.model
+
+import com.bindglam.felis.client.rendering.shader.Shader
+import com.bindglam.felis.utils.Destroyable
+import org.lwjgl.opengl.GL11
+import org.lwjgl.opengl.GL13
+import org.lwjgl.opengl.GL20
+import org.lwjgl.opengl.GL30
+import org.lwjgl.stb.STBImage
+import org.lwjgl.system.MemoryStack.stackPush
+import java.io.File
+import java.nio.ByteBuffer
+
+class Texture(file: File, private val type: Int, slot: Int, format: Int, pixelType: Int) : Destroyable {
+    val id = GL11.glGenTextures()
+
+    init {
+        var image: ByteBuffer? = null
+        var width = 0
+        var height = 0
+
+        stackPush().use { stack ->
+            val pComp = stack.mallocInt(1)
+            val pWidth = stack.mallocInt(1)
+            val pHeight = stack.mallocInt(1)
+
+            STBImage.stbi_set_flip_vertically_on_load(true)
+            image = STBImage.stbi_load(file.path, pWidth, pHeight, pComp, 4) ?: throw RuntimeException("Couldn't load ${file.path}")
+            width = pWidth.get()
+            height = pHeight.get()
+        }
+
+        GL13.glActiveTexture(slot)
+        bind()
+
+        GL11.glTexParameteri(type, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST_MIPMAP_LINEAR)
+        GL11.glTexParameteri(type, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST)
+
+        GL11.glTexParameteri(type, GL11.GL_TEXTURE_WRAP_S, GL11.GL_REPEAT)
+        GL11.glTexParameteri(type, GL11.GL_TEXTURE_WRAP_T, GL11.GL_REPEAT)
+
+        GL11.glTexImage2D(type, 0, GL11.GL_RGBA, width, height, 0, format, pixelType, image)
+        GL30.glGenerateMipmap(type)
+
+        STBImage.stbi_image_free(image!!)
+
+        unbind()
+    }
+
+    fun texUnit(shader: Shader, uniform: String, unit: Int) {
+        shader.activate()
+
+        GL20.glUniform1i(GL20.glGetUniformLocation(shader.id, uniform), unit)
+    }
+
+    fun bind() {
+        GL11.glBindTexture(type, id)
+    }
+
+    fun unbind() {
+        GL11.glBindTexture(type, 0)
+    }
+
+    override fun destroy() {
+        GL11.glDeleteTextures(id)
+    }
+}
